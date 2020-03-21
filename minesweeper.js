7fa6eaa6f51285a8a3a5d0dcfccbb8df8e2367a2
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-03-20 22:31:20 -0400

    Update minesweeper.js

--- minesweeper.js
+++ minesweeper.js
@@ -127,7 +127,7 @@ function roundSetup () {
 
 // On Block Click
   // TODO change back to false
-  $('.block[revealed="true"]').click(function() {
+  $('.block;).find('[revealed="true"]').click(function() {
     var x = $(this).attr("locationX");
     var y = $(this).attr("locationY");
     var isBomb = roundLookup(x, y, "isBomb");
