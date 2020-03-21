a933a3d4b6d20881d6ee4e9f9759938ae87000a8
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-03-20 22:07:55 -0400

    Update minesweeper.js

--- minesweeper.js
+++ minesweeper.js
@@ -137,7 +137,7 @@ function roundSetup () {
   for (i=0; i < 12; i++) {
     for (j=0; j < 12; j++) {
       var thisBomb = roundLookup(i, j, "isBomb");
-      $('#minesweeper').append('<div class="block" revealed="true" bomb=' + thisBomb + '></div>');
+      $('#minesweeper').find('.row').append('<div class="column block-container span1 sm"><div class="block" revealed="true" bomb=' + thisBomb + '></div></div>');
     }
   }
 }
